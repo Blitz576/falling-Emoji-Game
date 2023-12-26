190b52a645eef2552b00cbd2d4eb638c336984d4
Blitz576 <anagy8050@gmail.com>
2023-12-26 12:35:43 +0200

    added check image index properity to class cell

--- script/classes.js
+++ script/classes.js
@@ -4,6 +4,7 @@ class Cell{
         this.cell.style.width="50px";
         this.cell.style.height="50px";
         this.flag=0;
+        this.imageSources=["0","images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg"];   //possible images based 1 index
         //this.cell.style.backgroundColor="red";
     }
     appendToParent(parent){
@@ -11,8 +12,20 @@ class Cell{
     }
     appendImage(image){
         this.flag=1;
+        this.imageIndex = this.imageSources.indexOf(image.attributes.src);
         this.cell.appendChild(image);
     }
+    
+    
+    cellImageNumber(){
+      if(!this.flag)
+         return -1; //the cell has no image
+      else{
+         
+      }  
+
+    }
+
     isEmpty(){
         if(this.flag)
            return false;
